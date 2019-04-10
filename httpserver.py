2290f7cfc80cd132f1632c05d07146d5f9d69e19
jueane <jueane@live.com>
2019-04-10 11:14:30 +0800

    微调

--- httpserver.py
+++ httpserver.py
@@ -7,11 +7,13 @@ from requests import get
 
 RESP_OK = 'HTTP/1.1 200 OK\n'
 CONN_CLOSED = 'Connection: Closed\n'
+ip = '0.0.0.0'
+port = 8888
 
 
 def readfile(filename):
     try:
-        fo = open('webroot/'+filename, 'r+', encoding='utf-8')
+        fo = open('webroot'+filename, 'r+', encoding='utf-8')
         ret = fo.read()
         fo.close()
         ret = ret.encode("utf-8")
@@ -22,7 +24,7 @@ def readfile(filename):
 
 def readBinaryfile(filename):
     try:
-        fo = open('webroot/'+filename, 'rb')
+        fo = open('webroot'+filename, 'rb')
         ret = fo.read()
         fo.close()
         return ret
@@ -44,18 +46,14 @@ def process_body(path):
         contype = 'Content-Type: text/css\n'
         body = readfile(path)
     else:
-        if(path == ''):
-            path = 'index.html'
+        if(path == '/'):
+            path = '/index.html'
         contype = 'Content-Type: text/html\n'
         body = readfile(path)
     return contype, body
 
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = '0.0.0.0'
-
-port = 8888
-
 serversocket.bind((ip, port))
 serversocket.listen(10)
 
@@ -66,7 +64,7 @@ while True:
 
     # recv
     msg = clientsocket.recv(1024)
-    req = msg.decode('UTF-8')
+    req = msg.decode('utf-8')
 
     # req path
     strList = str.split(req, ' ', 2)
@@ -74,18 +72,15 @@ while True:
         continue
     path = strList[1]
     print('__req: '+path)
-    path = str.replace(path, '/', '', 1)
-
-    head = RESP_OK
-    head += CONN_CLOSED
 
     contype, body = process_body(path)
-    head += contype
 
     # resp
     if(body != None):
-        head += 'Content-Length: '
-        head += str(len(body))+'\n\n'
+        head = RESP_OK
+        head += CONN_CLOSED
+        head += contype
+        head += 'Content-Length: '+str(len(body))+'\n\n'
         head = head.encode('utf-8')
         clientsocket.send(head)
         clientsocket.send(body)
